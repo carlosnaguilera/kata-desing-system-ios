27e34607c9230fca9e3014d523a4e6503e1deab9
carlos <carlosnaguilera@gmail.com>
2024-08-24 20:48:36 +0200

    ♻️ use constants instead of variables in color palette

--- Sources/DesignSystem/DesignTokens/ColorPalette.swift
+++ Sources/DesignSystem/DesignTokens/ColorPalette.swift
@@ -5,10 +5,10 @@ extension Color {
     // We create these colors in case we can reuse them in other components apart from AlertView
     // to display errors, success or notifications
     // Otherwise they could be renamed to "errorAlertBackground", "errorAlertText", etc
-    static var errorBackground: Color { Color(.error).opacity(0.1) }
-    static var errorText: Color { Color(.error) }
-    static var successBackground: Color { Color(.success).opacity(0.1) }
-    static var successText: Color { Color(.success) }
-    static var notificationBackground: Color { Color(.notification).opacity(0.1) }
-    static var notificationText: Color { Color(.notification) }
+    static let errorBackground = Color(.error).opacity(0.1)
+    static let errorText = Color(.error)
+    static let successBackground = Color(.success).opacity(0.1)
+    static let successText = Color(.success)
+    static let notificationBackground = Color(.notification).opacity(0.1)
+    static let notificationText = Color(.notification)
 }
